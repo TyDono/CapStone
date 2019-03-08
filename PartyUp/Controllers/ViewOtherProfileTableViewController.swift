027a0ceaa84689d2110e78beb9b732e7fb99429b
Tyler Donohue <donohue_tyler@yahoo.com>
2019-03-08 08:39:30 -0700

    added constrains to otherTVC, they work fine constraints dded to the searchResultsTVC are not and still down work, functionality works. added actions and outlets, about to start on passing data into those outlets

--- PartyUp/Controllers/ViewOtherProfileTableViewController.swift
+++ PartyUp/Controllers/ViewOtherProfileTableViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class ViewOtherProfileTableViewController: UITableViewController {
-
+    
+    //MARK Outlets
+    @IBOutlet var otherTitleLabel: UILabel!
+    @IBOutlet var otherGameLabel: UILabel!
+    @IBOutlet var otherAgeLabel: UILabel!
+    @IBOutlet var otherGroupSizeLabel: UILabel!
+    @IBOutlet var otherExperianceLabel: UILabel!
+    @IBOutlet var otherAvailabilityLabel: UILabel!
+    @IBOutlet var otherAboutLabel: UILabel!
+    @IBOutlet var contactMe: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
       
     }
 
+    //MARK Actions
+    @IBAction func contactMeTapped(_ sender: Any) {
+    }
 }
