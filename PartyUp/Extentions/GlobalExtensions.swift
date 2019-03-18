cac6a2ed69392b1d8b94a9e394d470cdd0a31aea
Tyler Donohue <donohue_tyler@yahoo.com>
2019-03-18 11:14:02 -0600

    removed some files and added some more for Database, added a few comments on old code

--- PartyUp/Extentions/GlobalExtensions.swift
+++ PartyUp/Extentions/GlobalExtensions.swift
@@ -104,3 +104,20 @@ extension UIColor {
         )
     }
 }
+
+//scrollview constraints
+extension UIScrollView {
+    
+    var isAtBottom: Bool {
+        return contentOffset.y >= verticalOffsetForBottom
+    }
+    
+    var verticalOffsetForBottom: CGFloat {
+        let scrollViewHeight = bounds.height
+        let scrollContentSizeHeight = contentSize.height
+        let bottomInset = contentInset.bottom
+        let scrollViewBottomOffset = scrollContentSizeHeight + bottomInset - scrollViewHeight
+        return scrollViewBottomOffset
+    }
+    
+}
